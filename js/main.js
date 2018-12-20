38fe73118a2771470a028fccc14d7bd9157df6ee
Andrew Hall <andrew.hall@bglgroup.co.uk>
2018-12-20 12:30:28 +0000

    AH - Added stuff

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var TopDownGame = TopDownGame || {};
 
-TopDownGame.game = new Phaser.Game(160, 160, Phaser.AUTO, '');
+TopDownGame.game = new Phaser.Game(480, 240, Phaser.AUTO, '');
 
 TopDownGame.game.state.add('Boot', TopDownGame.Boot);
 TopDownGame.game.state.add('Preload', TopDownGame.Preload);
+TopDownGame.game.state.add('MainMenu', TopDownGame.MainMenu);
 TopDownGame.game.state.add('Game', TopDownGame.Game);
+TopDownGame.game.state.add('GameOver', TopDownGame.GameOver);
 
 TopDownGame.game.state.start('Boot');
